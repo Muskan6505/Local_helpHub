cb8c8fd1057b55304d0027dc89a96d0c157f2eda
Muskan6505 <muskan1144m@gmail.com>
2025-12-16 12:40:26 +0530

    feat: refactor server initialization and update CORS origin handling; add Netlify configuration

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,7 +1,8 @@
 import { app } from "../src/app.js";
 import { createServer } from "http";
 
+const server = createServer(app); 
+
 export default function handler(req, res) {
-    const server = createServer(app);
-    server.emit('request', req, res);
+    server.emit("request", req, res);
 }
